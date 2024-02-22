9c47417859ba49c1a13b4a3e0e5447fcf13c897b
ALANA SHARENI RAMÍREZ <aramirez38@ucateba.edu.do>
2024-02-22 03:00:37 -0400

    Yo hice el router y controller de orden

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ const app = express();
 import indexrouters from "./router/indexrouter.js";
 import clienterouters from "./router/clienterouter.js";
 import empleadorouters from "./router/empleado-router.js";
+import ordenrouters from "./router/ordenrouter.js";
 import detalleordenrouters from "./router/detalleordenrouter.js";
 //iniciar puerto principal
 
@@ -11,6 +12,7 @@ import detalleordenrouters from "./router/detalleordenrouter.js";
 app.use("/", indexrouters);
 app.use("/api/cliente/", clienterouters);
 app.use("/api/empleado/", empleadorouters);
+app.use("/api/orden/", ordenrouters);
 app.use("/api/detalleorden/", detalleordenrouters);
 
 // toca hacer los router de los demas
