c9d2a32b66a7a1df4af4dd64cf2e94d0bd1fdbdd
ALANA SHARENI RAMÍREZ <aramirez38@ucateba.edu.do>
2024-02-22 02:27:07 -0400

    Yo hice detalleordenrouter y detalleorden-Controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,14 @@ const app = express();
 import indexrouters from "./router/indexrouter.js";
 import clienterouters from "./router/clienterouter.js";
 import empleadorouters from "./router/empleado-router.js";
+import detalleordenrouters from "./router/detalleordenrouter.js";
 //iniciar puerto principal
 
 //routes
 app.use("/", indexrouters);
 app.use("/api/cliente/", clienterouters);
 app.use("/api/empleado/", empleadorouters);
+app.use("/api/detalleorden/", detalleordenrouters);
 
 // toca hacer los router de los demas
 app.listen(4000, () => console.log("Servidor ejecutado"));
