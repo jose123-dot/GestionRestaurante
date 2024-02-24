b0983e61e92e3fafeca65c086e38c7e2054ad5c7
jose123-dot <72807861+jose123-dot@users.noreply.github.com>
2024-02-23 22:42:26 -0400

    frontend con el menu

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Menu from "./Componentes/menu/menu.jsx";
+import Header from "./Componentes/header/header.jsx";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Menu></Menu>
+    </>
+  );
+}
+
+export default App;
