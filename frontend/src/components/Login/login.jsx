b567f5e9fe0cc7af22196baf42e6e9cc477defa7
jose123-dot <72807861+jose123-dot@users.noreply.github.com>
2024-02-28 15:24:10 -0400

    arregle el design del login

--- frontend/src/components/Login/login.jsx
+++ frontend/src/components/Login/login.jsx
@@ -1,90 +1,58 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 
 const Login = () => {
   //eventos
-  const [signupData, setSignupData] = useState({
-    username: "",
-    email: "",
-    password: "",
-    roles: [],
-  });
-  const [signinData, setSigninData] = useState({ email: "", password: "" });
-
-  const handleSignup = async () => {
-    try {
-      const response = await axios.post(
-        "http://localhost:4000/api/signup",
-        signupData
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.error("Error signing up:", error);
-    }
-  };
-
-  const handleSignin = async () => {
-    try {
-      const response = await axios.post(
-        "http://localhost:4000/api/signin",
-        signinData
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.error("Error signing in:", error);
-    }
-  };
 
   //return
   return (
     <>
-      <div>
-        <h2>Sign Up</h2>
-        <input
-          type="text"
-          placeholder="Username"
-          value={signupData.username}
-          onChange={(e) =>
-            setSignupData({ ...signupData, username: e.target.value })
-          }
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={signupData.email}
-          onChange={(e) =>
-            setSignupData({ ...signupData, email: e.target.value })
-          }
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={signupData.password}
-          onChange={(e) =>
-            setSignupData({ ...signupData, password: e.target.value })
-          }
-        />
-        <button onClick={handleSignup}>Sign Up</button>
+      <section className="vh-100">
+        <div className="container py-5 h-100">
+          <div className="row d-flex align-items-center justify-content-center h-100">
+            <div className="col-md-8 col-lg-7 col-xl-6">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+                className="img-fluid"
+                alt="imagen"
+              />
+            </div>
+            <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+              <h1>Login</h1>
+              <form>
+                {/* <!-- Email input --> */}
+                <div className="form-outline mb-4">
+                  <input
+                    type="email"
+                    id="form1Example13"
+                    className="form-control form-control-lg"
+                  />
+                  <label className="form-label" for="form1Example13"></label>
+                </div>
 
-        <h2>Sign In</h2>
-        <input
-          type="email"
-          placeholder="Email"
-          value={signinData.email}
-          onChange={(e) =>
-            setSigninData({ ...signinData, email: e.target.value })
-          }
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={signinData.password}
-          onChange={(e) =>
-            setSigninData({ ...signinData, password: e.target.value })
-          }
-        />
-        <button onClick={handleSignin}>Sign In</button>
-      </div>
+                {/* <!-- Password input --> */}
+                <div className="form-outline mb-4">
+                  <input
+                    type="password"
+                    id="form1Example23"
+                    className="form-control form-control-lg"
+                  />
+                  <label className="form-label" for="form1Example23">
+                    Password
+                  </label>
+                </div>
+
+                {/* <!-- Submit button --> */}
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-lg btn-block"
+                >
+                  Sign in
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
